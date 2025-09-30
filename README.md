47cfd278d5827bfa0c083b263b65e92cbf59e291
laudettekandie-eng <laudettekandie@gmail.com>
2025-09-30 14:29:50 +0300

    Add files via upload
    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Starnet Kenya Landing Page
+
+A simple React + Vite landing page for Starnet Kenya packages.
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "2rem", textAlign: "center" }}>
+      <h1>Starnet Kenya</h1>
+      <p>Affordable Internet Packages (Available on all networks)</p>
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        <li>1 Day - 7GB Ksh 79</li>
+        <li>7 Days - 12GB Ksh 89</li>
+        <li>5 Days - 10GB Ksh 99</li>
+        <li>14 Days - 25 GB Ksh 130</li>
+        <li>21 Days - 45GB Ksh 225</li>
+        <li>30 Days - Unlimited Ksh 299</li>
+      </ul>
+    </div>
+  );
+}
